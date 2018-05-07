5d9a8efa21ef8d64bb120188812d20045168d620
hanifveryconfusing@gmail.com <Wthanif_4869>
2018-05-08 00:10:56 +0800

    Initial commit

---
+++ app/src/main/java/android/vogella/de/shoppinglist/DatabaseHelper.java
@@ -0,0 +1,98 @@
+package android.vogella.de.shoppinglist;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by User on 7/5/2018.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Item.db";
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public void deleteData(int id,String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "DELETE FROM " + TABLE_NAME + " WHERE "
+                + COL_1 + " = '" + id + "'" +
+                " AND " + COL_2 + " = '" + name + "'";
+        Log.d("msg", "deleteName: query: " + query);
+        Log.d("msg", "deleteName: Deleting " + name + " from database.");
+        db.execSQL(query);
+    }
+
+//    public void deleteData(ArrayList<Integer> selectedID, ArrayList<String> selectedName){
+//        SQLiteDatabase db = this.getWritableDatabase();
+//        for(int i = 0;i< selectedID.size();i++) {
+//            String query = "DELETE FROM " + TABLE_NAME + " WHERE "
+//                    + COL_1 + " = '" + selectedID.get(i) + "'" +
+//                    " AND " + COL_2 + " = '" + selectedName.get(i) + "'";
+//            Log.d("msg", "deleteName: query: " + query);
+//            Log.d("msg", "deleteName: Deleting " + selectedName.get(i) + " from database.");
+//            db.execSQL(query);
+//        }
+//    }
+
+    public void deleteData(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "DELETE FROM " + TABLE_NAME + " WHERE "
+                + COL_1 + " = '" + id + "'";
+        Log.d("msg", "deleteName: query: " + query);
+        db.execSQL(query);
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("SELECT * FROM " + TABLE_NAME,null);
+        return res;
+    }
+
+    public Cursor getItemID(String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT " + COL_1 + " FROM " + TABLE_NAME +
+                " WHERE " + COL_2 + " = '" + name + "'";
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+
+
+    public void removeAll(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(DatabaseHelper.TABLE_NAME, null,null);
+
+    }
+}
---
+++ app/src/main/java/android/vogella/de/shoppinglist/MainActivity.java
@@ -0,0 +1,152 @@
+package android.vogella.de.shoppinglist;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import java.util.HashSet;
+import java.util.Set;
+import android.app.AlertDialog;
+import android.widget.EditText;
+import android.content.DialogInterface;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.app.Activity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> shoppingList = null;
+    ArrayAdapter<String> adapter = null;
+    ListView lv = null;
+    DatabaseHelper myDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        myDB = new DatabaseHelper(this);
+
+        shoppingList = new ArrayList<>();
+        Collections.addAll(shoppingList, "Eggs", "Yogurt", "Milk", "Bananas", "Apples", "Tide with bleach", "Cascade");
+        shoppingList.addAll(Arrays.asList("Napkins", "Dog food", "Chapstick", "Bread"));
+        shoppingList.add("Sunscreen");
+        shoppingList.add("Toothpaste");
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, shoppingList);
+        lv = (ListView) findViewById(R.id.listView);
+        lv.setAdapter(adapter);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.action_sort) {
+            Collections.sort(shoppingList);
+            lv.setAdapter(adapter);
+            return true;
+        }
+        if (id == R.id.action_add){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Add Item");
+            final EditText input = new EditText(this);
+            builder.setView(input);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    shoppingList.add(preferredCase(input.getText().toString()));
+                    Collections.sort(shoppingList);
+                    lv.setAdapter(adapter);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+            return true;
+
+        }
+        if (id == R.id.action_clear){
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Clear Entire List");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    shoppingList.clear();
+                    lv.setAdapter(adapter);
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public static String preferredCase(String original)//Capitalize first letter and uncapitalize the rest
+    {
+        if (original.isEmpty())
+            return original;
+
+        return original.substring(0, 1).toUpperCase() + original.substring(1).toLowerCase();
+    }
+
+    public void addItem(String name){
+        boolean isInserted = myDB.insertData(name);
+        if(isInserted == true){
+            Toast.makeText(MainActivity.this,"Item added", Toast.LENGTH_LONG).show();
+        }
+        else
+            Toast.makeText(MainActivity.this,"Item not added", Toast.LENGTH_LONG).show();
+
+
+    }
+
+}
